585508560fafa46dd8499515408bf32ca8131fd4
Rutvik <rutvikraut567@gmail.com>
2024-12-14 13:43:13 +0530

    changed backend url

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 
 export const fetchData = async (url)=>{
     try{
-        const BASE_URL = 'http://localhost:5050/api';
+        const BASE_URL = 'https://e-commerce-product-cart-backend.vercel.app';
         const res = await fetch(`${BASE_URL}${url}`);
         if (!res.ok) {
         throw new Error(`HTTP error! status: ${res.status}`);
