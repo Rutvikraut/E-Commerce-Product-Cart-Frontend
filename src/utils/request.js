3f98948b7cd6ef8de7d659cf857501b4e2ba2144
Rutvik <rutvikraut567@gmail.com>
2024-12-14 14:02:19 +0530

    update fetch

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 
 export const fetchData = async (url)=>{
     try{
-        const BASE_URL = 'https://e-commerce-product-cart-backend.vercel.app';
+        const BASE_URL = 'https://e-commerce-product-cart-backend.vercel.app/api';
         const res = await fetch(`${BASE_URL}${url}`);
         if (!res.ok) {
         throw new Error(`HTTP error status: ${res.status}`);
